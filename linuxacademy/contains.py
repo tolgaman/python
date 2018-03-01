23047377619a6fb2b6802770b3b88f75fcdeeff7
Tolga <tolga.ozaltin@gmail.com>
2018-03-01 01:11:35 +0000

    Added a word contain program

---
+++ linuxacademy/contains.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import argparse
+parser = argparse.ArgumentParser(description='Search for words including partial word')
+parser.add_argument('snippet', help='partial (or complete) string to search for in the words file')
+
+args = parser.parse_args()
+snippet = args.snippet.lower()
+
+words = open('/usr/share/dict/words').readlines()
+matches = []
+for word in words:
+      if snippet in word.lower():
+         matches.append(word)
+print(matches)
